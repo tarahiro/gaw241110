0ebebe95d0a35a63afc8f2e688931f7cd41d6492
tarahiro <38371023+tarahiro@users.noreply.github.com>
2024-11-07 15:16:21 +0900

    Initial commit

---
+++ Assets/Tarahiro/Script/Sound/ISeMaster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Tarahiro.Sound
+{
+    public interface ISeMaster : IIdentifiable, IIndexable
+    {
+
+        /// <summary>
+        /// このデータのIDを取得します。
+        /// </summary>
+        string Id { get; }
+
+        string SePath{ get; }
+    }
+}
